55ea0c5e417c0c3b948d9e0f0bfdab99af13d36c
cyberpunk1971 <adt71@cox.net>
2018-08-31 10:35:19 -0700

    WIP styling needed. Logic working

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-
-function renderArray() {
-  $('.js-shopping-list').append(
-    `<li>${myArray}</li>`
-  )
-};
-let myArray = [];
-
-$('#js-add-item').on('click', function (e) {
-  e.preventDefault();
-  let item = $('.js-shopping-list-entry').val();
-  myArray.push(item);
-  console.log(myArray);
-  renderArray();
+$(function () {
+  $('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+    const userInput = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    console.log(userInput);
+    $('.js-shopping-list').append(
+      `<li class="js-list-item" id="js-list-item">${userInput}<span>
+      <button id="js-delete">Delete</button></span>
+      <button id="js-check">Check</button></span>
+      </li>`
+    )
+    $('#js-check').on('click', function () {
+      $('.js-list-item').toggleClass('js-item');
+    });
+    $('#js-delete').on('click', function () {
+      $('.js-list-item').remove();
+    })
+  })
 })
